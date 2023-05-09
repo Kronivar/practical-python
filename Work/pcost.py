0d2f7f03d131b32f636ec74389aa4ef73f73aab8
Kronivar <t.xristos@hotmail.com>
2023-05-09 22:33:24 +0200

    End of chapter 1

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,28 @@
 # pcost.py
 #
 # Exercise 1.27
+import csv
+import sys
+
+def portfolio_cost(filename):
+    total = 0
+    with open(filename, 'r') as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for row in rows:
+            try:
+                ticker, shares, price = row
+                total += (int(shares) * float(price))
+            except ValueError:
+                print(f"Invalid number for {ticker}.\nShares: {shares}\nPrice: {price}")
+                continue
+            
+    return total
+    
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = r'Work\Data\portfolio.csv'
+
+cost = portfolio_cost(filename)
+print(f'Total cost: {cost}')
